2d44f8bd3df13db7d74772df1d493ae6551218bf
samr <samr.stuff@pm.me>
2023-02-10 21:31:08 -0800

    egg

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -7,21 +7,12 @@ Thank you for your interest in contributing to the szia project!
 If you'd like to contribute to szia,
 there are multiple ways to start contributing to the szia project.
 
-NOTE: All changes should be made in the `trunk.php` file.
-
 ### Open a pull request
 
 If you'd like to add a feature to szia,
 you can open a pull request at the
 [szia-oe](https://github.com/samrpf/szia-oe)
 repository.
-
-### Become a szia core member
-
-If you'd like to start working on szia directly,
-you can email us at
-[szia-core-team@googlegroups.com](mailto:szia-core-team@googlegroups.com?subject=szia%20core%20request)
-to become a szia core member.
 
 ## Ways to contribute
 
@@ -47,10 +38,3 @@ Self-explanatory.
 
 You could add any feature to the home screen
 just to make szia feel more like an operating environment.
-
-### Do a bounty
-
-In `BOUNTY.md`, there are always new things to do which we want done.
-
-These could be issues with the current environment,
-or small features we'd like to see added.
--- index.php
+++ index.php
@@ -1,48 +1,68 @@
 <!--
 szia
-Copyright (c) 2022 samrpf. See the LICENSE file for more information.
-
-v0.2.0. See the CHANGELOG.md file for more information.
-
-█▓▒▒░░░ szia ░░░▒▒▓█
+Copyright (c) 2023 samrpf. See the LICENSE file for more information.
 -->
 
-<!-- SETUP START -->
+<title>szia workstation</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta charset="UTF-8" />
 
-<link rel="favicon" type="image/png" href="/favicon.png" />
-
-<!-- get swal -->
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<!-- get bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>
+
+<script>
+	const popups = {
+		about: function() {
+			swal('About', 'szia is a project by samrpf to create an online operating environment based on zlinux.\nYou can find zlinux at https://zlinux.mkcodes.repl.co.');
+		},
+		credits: function() {
+			swal('Credits', 'Base - zlinux (https://zlinux.mkcodes.repl.co)');
+		},
+		license: function() {
+			swal('License', 'This piece of software follows the terms of the MIT License.\nYou can find it in the LICENSE file.');
+		}
+	};
+
+	function updateTime() {
+  		const timeElapsed = Date.now();
+  		const todayObject = new Date(timeElapsed);
+  		const today = todayObject.toUTCString();
+  		document.getElementById("time").innerHTML = today;
+		setTimeout(updateTime, 1000);
+	}
+	
+	updateTime();
+</script>
 
 <style>
-	/* using Roboto in this particular project */
 	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap');
-
+	
 	body {
-		font-family: Roboto, sans-serif;
+		font-family: 'Roboto', sans-serif;
 		font-weight: 400;
-		/* set background */
-		background-image: url("/background.jpg");
-		/* since the text is going to be on a dark background, it should be light */
+		background-image: url('background.jpg');
+		color: white;
+		margin-bottom: -1px;
+  		-webkit-background-clip: padding-box; /* for Safari */
+  		background-clip: padding-box; /* for IE9+, Firefox 4+, Opera, Chrome */
+		overflow: hidden;
+	}
+
+	#menu a {
 		color: white;
 	}
 
-	#trunk-link {
+	#menu a:hover {
 		color: white;
 	}
 
-	#trunk-link:hover {
-		color: lightblue;
-	}
-
-	#trunk-link:visited {
+	#menu a:visited {
 		color: white;
 	}
 	
-	#menu	{
-  	position: absolute;
+	#menu {
+  		position: absolute;
 		top: 5px;
 		right: 5px;
 		text-align: right;
@@ -59,99 +79,163 @@ v0.2.0. See the CHANGELOG.md file for more information.
 	}
 </style>
 
-<!-- BODY START -->
-
-<!-- menu -->
 <div id="menu">
-	<!-- title! -->
 	<p id="menu-title">szia</p>
-	<!-- trunk/release switcher -->
-	<p><a id="trunk-link" href="/trunk.php">switch to trunk</a></p>
-	<!-- time box -->
-	<div id="time">::placeholder::</div>
-	<script>
-		function updateTime() {
-  		const timeElapsed = Date.now();
-  		const todayObject = new Date(timeElapsed);
-  		const today = todayObject.toUTCString();
-  		document.getElementById("time").innerHTML = today;
-			setTimeout(updateTime, 1000);
-		}
+
+	<div>
+		<a href="" onclick="popups.about()">about</a>
+		|
+		<a href="" onclick="popups.credits()">credits</a>
+		|
+		<a href="" onclick="popups.license()">license</a>
+	</div>
 	
-		function sleep(milliseconds) {
-  		const date = Date.now();
-  		let currentDate = null;
-  		do {
-    		currentDate = Date.now();
-  		} while (currentDate - date < milliseconds);
-		}
-		
-		updateTime();
-	</script>
+	<div id="time">::placeholder::</div>
 </div>
 
-<!-- window management and button placement -->
-
-<p>Window management is currently a trunk feature.</p>
-
-<!-- toolbar -->
-
-<?php
-	/* function add2bar
-	 * used to add buttons to the toolbar
-	 * - barfn :: javascript function ran when button clicked
-	 * - barname :: name that shows on button
-	 */
-	function add2bar($barname, $barfn) {
-  	echo '    <button type="button" class="btn btn-secondary" onclick="'.$barfn.'">'.$barname.'</a></button>';
-	}
-?>
-
-<script>
-	/* functions used in toolbar */
-	function openp(url) {
-		window.location.href = url;
-	}
-	function showAbout() {
-		swal('about', 'v0.2.0\n\nszia is a project by samrpf to create an online operating environment based on zlinux.\nYou can find zlinux at https://zlinux.mkcodes.repl.co.');
-	}
-	function showCredits() {
-		swal('Credits', 'Base - zlinux (https://zlinux.mkcodes.repl.co)');
-	}
-	function showLicense() {
-		swal('License', 'This piece of software follows the terms of the MIT License.\nYou can find at in the LICENSE file.');
-	}
-</script>
-
-
 <style>
-	* {
-		opacity: 90%;
-	}
+	.szia-window {
+    	outline: 1px solid white;
+		backdrop-filter: blur(2px);
+    	position: absolute;
+    	top: 0;
+    	left: 50%;
+		width: 500px;
+		height: 500px;
+		min-height: 100px;
+		min-width: 100px;
+		resize: both;
+		overflow: auto;
+		max-height: fit-content;
+		max-width: fit-content;
+		filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.2));
+  	}
 	
-	body {
-		opacity: 100%;
+	iframe {
+    	outline: none;
+    	border: none;
+    	resize: both;
+    	overflow: auto;
+  		position: auto;
+  	}
+
+	.szia-window-button {
+		border: 1px solid rgba(255, 255, 255, 0.7);
+  		outline: none;
+    	background-color: transparent;
+  		color: white;
+    	position: relative;
+		transition: ease 0.5s;
+		width: 10%;
 	}
-	
-	body {
-  	margin-bottom: -1px;
-  	-webkit-background-clip: padding-box; /* for Safari */
-  	background-clip: padding-box; /* for IE9+, Firefox 4+, Opera, Chrome */
+
+	.szia-window-button:hover {
+		width: 20%;
+		background-color: rgba(0, 0, 0, 0.75);
 	}
-	
-	.center {
-		//position fixed
-		bottom: 100%;
+
+	.szia-window-control {
+		position: absolute;
+		right: 0;
+		font-family: 'Consolas', 'Courier New', monospace;
+	}
+
+	.szia-window-frame {
+		width: 100%;
+		height: 100%;
 	}
 </style>
 
-<div class="btn-toolbar center fixed-bottom" role="toolbar" aria-label="Toolbar with button groups">
-	<div class="btn-group mr-2" role="group" aria-label="First group">
-		<?php
-			add2bar('about', 'showAbout();');
-			add2bar('credits', 'showCredits();');
-			add2bar('license', 'showLicense();');
-			add2bar('samrpf', 'openp("https://samrpf.wordpress.com");');
-		?>
-	</div>
-</div>
+<script>
+	class SziaWindow {
+		constructor(id, name, url) {
+			this.id = id;
+			this.name = name;
+			this.url = url;
+
+			this.showing = false;
+		}
+
+		show() {
+			document.getElementById(this.id + "-frame").src = this.url;
+			document.getElementById(this.id + "-window").style.display = "block";
+			document.getElementById(this.id + "-button").style.background = "#91c4ed";
+		}
+
+		hide() {
+			document.getElementById(this.id + "-frame").src = "about:blank";
+			document.getElementById(this.id + "-window").style.display = "none";
+			document.getElementById(this.id + "-button").style.background = "transparent";
+		}
+
+		load() {
+			// if window is showing, hide it, else show it again
+			if (this.showing) {
+				this.showing = false;
+				this.hide();
+			} else {
+				this.showing = true;
+				this.show();
+			}
+		}
+	}
+</script>
+
+<?php
+	$szia_script = '
+		<script>
+			let %szia-id% = new SziaWindow("%szia-id%", "%szia-name%", "%szia-url%");
+		</script>
+
+		<button
+		    type="button"
+		    id="%szia-id%-button"
+			class="szia-window-button"
+		    onclick="%szia-id%.load()">
+		    %szia-name%
+		</button>
+
+		<br />
+
+		<div
+		  	id="%szia-id%-window"
+		  	class="ui-widget-content szia-window">
+
+		  	<span>%szia-name%</span>
+
+		  	<a
+		      	onclick="%szia-id%.hide()"
+				class="szia-window-control">
+		    	[-]
+			</a>
+
+			<iframe
+				id="%szia-id%-frame"
+				src="about:blank"
+				class="szia-window-frame" />
+		</div>
+
+		<script>
+		    %szia-id%.hide();
+		</script>
+	';
+
+	function add($id, $appName, $appURL, $scriptFormat) {
+		$add_script = str_replace("%szia-id%", $id, $scriptFormat);
+		$add_script = str_replace("%szia-name%", $appName, $add_script);
+		$add_script = str_replace("%szia-url%", $appURL, $add_script);
+
+		echo $add_script;
+	}
+
+	add('https://bing.com', 'Bing', 'bingapp', $szia_script);
+	add('browser/browser.html', 'Browser', 'wbr', $szia_script);
+	add('notes/notes.html', 'Notes', 'noteapp', $szia_script);
+	add('helpapp/helpapp.html', 'Help', 'helpapp', $szia_script);
+?>
+
+<script>
+	$('.szia-window').draggable();
+	$('.szia-window').resizable();
+	$('.szia-window-button').draggable({cancel: false});
+</script>
